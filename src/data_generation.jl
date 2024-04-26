ac3d71fd1d4aefd3a0ec5a57f8c9f2ebc014f7ee
KumarRishabh <rkumar5@ualbert.ca>
2024-04-26 11:18:20 -0600

    Added updating state equation to the Julia code

--- src/data_generation.jl
+++ src/data_generation.jl
@@ -2,45 +2,77 @@
 
 using Random
 using Plots
+using Distributions
 
 # Initialize parameters
-width, height = 50, 50  # dimensions of the grid
-prob_infect = 0.2       # probability of infecting a neighbor
-prob_recover = 0.1      # probability of recovery
+width, height = 50, 50 # dimensions of the grid
+# add paddings to the grid 
+width_with_padding = width + 2
+height_with_padding = height + 2
+# prob_infect = 0.2       # probability of infecting a neighbor
+infection_rate = 2 # a
+# prob_recover = 0.1 
+recovery_rate = 1 # s   # probability of recovery
 prob_noise = 0.05       # probability of observation noise
 num_steps = 300       # number of time steps to simulate
 
 # Initialize the grid
-state = zeros(Bool, width, height)
+state = zeros(Bool, width_with_padding, height_with_padding)
 initial_infections = 10  # start with 10 infected individuals
 for _ in 1:initial_infections
     state[rand(1:width), rand(1:height)] = true
 end
 
 # Function to update the state of the grid
-function update_state(state)
-    new_state = copy(state)
-    for i in 1:width
-        for j in 1:height
-            if state[i, j] == 1 # if the node is currently infected
-                # Recovery process
-                if rand() < prob_recover
-                    new_state[i, j] = false
-                end
-                
-                # Infection process
-                neighbors = [(i-1) % width + 1, (i+1) % width + 1,
-                             (j-1) % height + 1, (j+1) % height + 1]
-                for (ni, nj) in [(i, neighbors[3]), (i, neighbors[4]), (neighbors[1], j), (neighbors[2], j)]
-                    if rand() < prob_infect
-                        new_state[ni, nj] = true
-                    end
-                end
-            end
+
+
+
+function calculate_all_rates(state)
+    # After calculating the rates, we need to select the position of the node 
+    # which will be updated
+    global width_with_padding, height_with_padding
+    rates = zeros(width_with_padding, height_with_padding)
+    for i in 2:width_with_padding-1
+        for j in 2:height_with_padding-1
+            rates[i, j] = infection_rate *(1 - state[i, j]) * (state[i, j+ 1] + state[i, j - 1] + state[i - 1, j] + state[i + 1, j]) + recovery_rate * state[i, j]
         end
     end
-    return new_state
+    # choose a grid position according to the rate 
+
 end
+
+function sample_time_with_rates(state, rates)
+    global width_with_padding, height_with_padding
+    rate = sum(rates)
+    # sample from the Exponential rate
+    time = rand(Exponential(1 / rate))
+    return time
+end
+
+function update_states!(state, rates)
+    global width_with_padding, height_with_padding
+    r = rand()
+    cum_rate = 0.0
+    total_rate = sum(rates)
+    for i in 2:width_with_padding-1
+        for j in 2:height_with_padding-1
+            cum_rate += rates[i, j]
+            if cum_rate >= r * total_rate
+                state[i, j] = !state[i, j]
+                break
+            end
+        end
+        if cum_rate >= r * total_rate
+            break
+        end
+    end
+    return (i, j)
+end
+
+function update_rates!(state, rates)
+# rate = calculate_rates(state)
+    # calculate the rate for the min(Exponential) [addition of rates]
+    # sample from the Exponential rate
 
 # Function to add noise to observations
 function add_noise(state)
@@ -68,16 +100,18 @@ function generate_animation!(state, num_steps)
 
     anim = @animate for step in 1:num_steps
         state = update_state(state)
-        noisy_state = add_noise(state)
+        # noisy_state = add_noise(state)
         
         # clf(p)
-        heatmap!(p, noisy_state, color=:grays, lengend = false)
+        heatmap!(p, state, color=:grays, lengend = false)
+        # Put time step in the title
+        title!("Time step: $step")
     end
     return anim
 end
 
 @time anim = generate_animation!(state, num_steps)
-gif(anim, "contact_process_tori.gif", fps=10)
+gif(anim, "contact_process_tori.gif", fps=2)
 println("Simulation completed.")
 # gif(anim, "contact_process_tori.gif", fps=10)
 # println("Simulation completed.")
