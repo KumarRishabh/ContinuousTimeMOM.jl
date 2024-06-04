f47fc2a1b24dd611be5e3b02f7559b9b2e7599a0
KumarRishabh98 <rkumar5@ualberta.ca>
2024-06-04 12:12:15 -0600

    Added residual and stratified filters

--- tests/test_branching_particle_filter.jl
+++ tests/test_branching_particle_filter.jl
@@ -14,12 +14,8 @@ dir_path = joinpath(@__DIR__, "../src/seed_42_num_particles_15k")
 # typeof(dir_path)
 Random.seed!(42) # earlier seed was 10
 save_fig_path = joinpath(@__DIR__, "../src/seed_42_num_particles_18k/figures")
-Random.seed!(1)
-dir_path = joinpath(@__DIR__, "../src/filtered_data")
-save_fig_path = joinpath(@__DIR__, "../src/figures")
 grid_params = ContactProcess.GridParameters(width=10, height=10) # changed to 15 × 15 grid
 model_params = ContactProcess.ModelParameters(infection_rate=0.05, recovery_rate=0.1, time_limit=75, prob_infections=0.3, num_simulations=1000) # rates are defined to be per day
-model_params = ContactProcess.ModelParameters(infection_rate=0.05, recovery_rate=0.1, time_limit=100, prob_infections=0.3, num_simulations=1000) # rates are defined to be per day
 
 state, rates = ContactProcess.initialize_state_and_rates(grid_params, model_params; mode="fixed_probability")
 state_sequence, transition_times, updated_nodes = ContactProcess.run_simulation!(state, rates, grid_params, model_params)
@@ -28,13 +24,13 @@ observed_state
 observations, observation_time_stamps = get_observations_from_state_sequence(state_sequence, model_params.time_limit, transition_times)
 observed_dict = Dict(observation_time_stamps .=> observations)
 initial_num_particles = 15000 # initially start with 100 particles
-initial_num_particles = 14000 # initially start with 100 particles
 # between the observation time stamps, simulate the contact process with s tate and rates
 V = 0.2 * rand() - 0.1
 U = 0.2 * rand() - 0.1
 
 # initialize_particles(initial_num_particles, grid_params, model_params)
 
+CTMOM.combined_branching_particle_filter(initial_num_particles, grid_params, model_params, observation_time_stamps, observations, r=4, signal_state=state, signal_rate=rates, testing_mode = false, dir_path=dir_path)
 CTMOM.combined_branching_particle_filter(initial_num_particles, grid_params, model_params, observation_time_stamps, observations, r=4, signal_state=state, signal_rate=rates, testing_mode = false, dir_path=dir_path)
 
 # total_particles(particle_history, 0.0)
@@ -63,12 +59,36 @@ function calculate_error(estimate, actual_signal)
     error = 0
     for i in 2:(size(estimate, 1) - 1)
         for j in 2:size(estimate, 2) - 1
+    for i in 2:(size(estimate, 1) - 1)
+        for j in 2:size(estimate, 2) - 1
             error += (estimate[i, j] - actual_signal[i, j])^2
         end
     end
 
     return error / ((size(estimate, 1) - 2) * ((size(estimate, 2) - 2)))
+    return error / ((size(estimate, 1) - 2) * ((size(estimate, 2) - 2)))
 end
+
+# implement a cuda kernel to calculate the error 
+# function calculate_error_kernel(estimate::CuDeviceArray, actual_signal::CuDeviceArray, error::CuDeviceArray)
+#     i = threadIdx().x + 1
+#     j = threadIdx().y + 1
+
+#     if i > 1 && i < size(estimate, 1) && j > 1 && j < size(estimate, 2)
+#         @inbounds error[i, j] = (estimate[i, j] - actual_signal[i, j])^2
+#     end
+
+#     return
+# end
+
+# function calculate_error(estimate::CuDeviceArray, actual_signal::CuDeviceArray)
+#     error = CUDA.zeros(size(estimate))
+#     numblocks = (size(estimate, 1), size(estimate, 2))
+#     numthreads = (1, 1)
+#     @cuda threads=(numthreads) blocks=(numblocks) calculate_error_kernel(estimate, actual_signal, error)
+
+#     return sum(error) / ((size(estimate, 1) - 2) * ((size(estimate, 2) - 2)))
+# end
 
 # implement a cuda kernel to calculate the error 
 # function calculate_error_kernel(estimate::CuDeviceArray, actual_signal::CuDeviceArray, error::CuDeviceArray)
@@ -107,6 +127,8 @@ function calculate_error_trajectory(observation_time_stamps, state_sequence, tra
 end
 observation_time_stamps[1]
 errors = calculate_error_trajectory(observation_time_stamps, state_sequence, transition_times, time_limit=model_params.time_limit)
+plot(observation_time_stamps[1:length(errors)], errors, label="Mean Squared Error", xlabel="Time", ylabel="Mean Squared Error", title="Infection grid error", legend=:topleft)
+savefig("$save_fig_path/error_plot_stratified_filter(2).png")
 plot(observation_time_stamps[1:length(errors)], errors, label="Mean Squared Error", xlabel="Time", ylabel="Mean Squared Error", title="Infection grid error", legend=:topleft)
 savefig("$save_fig_path/error_plot_stratified_filter(2).png")
 
